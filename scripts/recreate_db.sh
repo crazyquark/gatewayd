f83cf249ce17960dfe8e312550d3cc5f6894f69a
crazyquark <sam.bitcoin@gmail.com>
2015-05-26 11:33:34 +0300

    VSCode stuff + DB regenerate script fix.

--- scripts/recreate_db.sh
+++ scripts/recreate_db.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# run this from the root folder
 sudo -u postgres psql -c "drop database gatewayd_db"
 sudo -u postgres psql -c "create database gatewayd_db with owner gatewayd_user encoding='utf8'"
 grunt migrate
+# info:  address=rBCPdrn7HnY8XB4dc674316iFq8s3s7F4, secret=shKAuRZkALboqFsiapGvzQXJfzLHf
+bin/gateway set_cold_wallet rBCPdrn7HnY8XB4dc674316iFq8s3s7F4
 
