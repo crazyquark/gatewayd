d7fd1c90593065341f7403510948fa323524ccef
crazyquark <sam.bitcoin@gmail.com>
2015-05-28 16:23:23 +0300

    Add helper script to fund cold wallet. Fix set trust line gatewayd API for wizard usage.

---
+++ scripts/fund_cold_wallet.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+command -v js24 >/dev/null || { sudo apt-get install libmozjs-24-bin } 
+result=`curl -X POST -d '{ "method": "sign", "params": [ { "offline": false, "secret": "masterpassphrase", "tx_json": { "Account": "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh", "Amount": "999000000", "Destination": "r4gzWvzzJS2xLuga9bBc3XmzRMPH3VvxXg", "TransactionType": "Payment" } } ] }' http://localhost:5005/`
+tx_blob=`echo $result | ./jsawk -j js24 'return this.result.tx_blob'`
+curl -X POST -d '{ "method": "submit", "params": [ { "tx_blob": "'"$tx_blob"'" } ] }' http://localhost:5005/
