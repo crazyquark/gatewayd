0073d118dac5ac21a477c0589382f8a4c5fa3f0f
crazyquark <sam.bitcoin@gmail.com>
2015-06-23 10:49:12 +0300

    Don't automatically set cold wallet, let the wizard do it

--- scripts/recreate_db.sh
+++ scripts/recreate_db.sh
@@ -4,5 +4,5 @@ sudo -u postgres psql -c "drop database gatewayd_db"
 sudo -u postgres psql -c "create database gatewayd_db with owner gatewayd_user encoding='utf8'"
 grunt migrate
 # info:  address=rBCPdrn7HnY8XB4dc674316iFq8s3s7F4, secret=shKAuRZkALboqFsiapGvzQXJfzLHf
-bin/gateway set_cold_wallet rBCPdrn7HnY8XB4dc674316iFq8s3s7F4
+#bin/gateway set_cold_wallet rBCPdrn7HnY8XB4dc674316iFq8s3s7F4
 
