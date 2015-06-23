315a9a79bde287c6bbd251d67b62ce10e280248a
crazyquark <sam.bitcoin@gmail.com>
2015-06-23 16:31:10 +0300

    Fixed root acct addr

--- scripts/no_ripple.sh
+++ scripts/no_ripple.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Refernce here: https://wiki.ripple.com/Sending_RPC_Commands
-curl -X POST -d '{ "method": "submit", "params": [ { "secret": "masterpassphrase", "tx_json": { "TransactionType": "AccountSet", "Account" : "rHb10CJAWyB4rj91VRWn96DkukG4bwdtyTh", "SetFlag": "8" } } ] }' http://localhost:5005/
+curl -X POST -d '{ "method": "submit", "params": [ { "secret": "masterpassphrase", "tx_json": { "TransactionType": "AccountSet", "Account" : "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh", "SetFlag": "8" } } ] }' http://localhost:5005/
 
 
