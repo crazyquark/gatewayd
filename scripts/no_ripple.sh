da85bda1550fbb0d9fddf7226da61fb7bd4e9b7d
crazyquark <sam.bitcoin@gmail.com>
2015-06-25 10:29:10 +0300

    Make no_ripple generic, now can take account as arg

--- scripts/no_ripple.sh
+++ scripts/no_ripple.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Refernce here: https://wiki.ripple.com/Sending_RPC_Commands
-curl -X POST -d '{ "method": "submit", "params": [ { "secret": "masterpassphrase", "tx_json": { "TransactionType": "AccountSet", "Account" : "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh", "SetFlag": "8" } } ] }' http://localhost:5005/
+ACCOUNT="$1"
+if [ -z $ACCOUNT ]
+        then
+                ACCOUNT="rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh"
+fi
+
+curl -X POST -d '{ "method": "submit", "params": [ { "secret": "masterpassphrase", "tx_json": { "TransactionType": "AccountSet", "Account" : "'"$ACCOUNT"'", "SetFlag": "8" } } ] }' http://localhost:5005/
 
 
