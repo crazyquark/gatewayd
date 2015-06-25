9b364c5b85dfb38dd14cbd05b8271c823ccdd171
crazyquark <sam.bitcoin@gmail.com>
2015-06-25 16:20:15 +0300

    no ripple on cold wallet. add secret

--- scripts/no_ripple.sh
+++ scripts/no_ripple.sh
@@ -1,11 +1,16 @@
-#!/bin/bash
+!/bin/bash
 # Refernce here: https://wiki.ripple.com/Sending_RPC_Commands
 ACCOUNT="$1"
+SECRET="$2"
 if [ -z $ACCOUNT ]
         then
-                ACCOUNT="rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh"
+                ACCOUNT="r4gzWvzzJS2xLuga9bBc3XmzRMPH3VvxXg"
+fi
+if [ -z $SECRET ]
+	then
+		SECRET="shrixAzTwHCDGfYTMUFcqhwdWVzCp"
 fi
 
-curl -X POST -d '{ "method": "submit", "params": [ { "secret": "masterpassphrase", "tx_json": { "TransactionType": "AccountSet", "Account" : "'"$ACCOUNT"'", "SetFlag": "8" } } ] }' http://localhost:5005/
+curl -X POST -d '{ "method": "submit", "params": [ { "secret": "shrixAzTwHCDGfYTMUFcqhwdWVzCp", "tx_json": { "TransactionType": "AccountSet", "Account" : "'"$ACCOUNT"'", "SetFlag": "8" } } ] }' http://localhost:5005/
 
 
