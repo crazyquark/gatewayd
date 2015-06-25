2e3ab45c57e0ab7a3145dabfc9c852cf53d82e2e
crazyquark <sam.bitcoin@gmail.com>
2015-06-25 10:34:49 +0300

    Fixes for bash

--- scripts/fund_account.sh
+++ scripts/fund_account.sh
@@ -9,5 +9,9 @@ fi
 command -v js24 > /dev/null || sudo apt-get install libmozjs-24-bin  
 result=`curl -X POST -d '{ "method": "sign", "params": [ { "offline": false, "secret": "masterpassphrase", "tx_json": { "Account": "'"$ACCOUNT"'", "Amount": "999000000", "Destination": "r4gzWvzzJS2xLuga9bBc3XmzRMPH3VvxXg", "TransactionType": "Payment" } } ] }' http://localhost:5005/`
 tx_blob=`echo $result | ./jsawk -j js24 'return this.result.tx_blob'`
-echo  "tx_blob is: $tx_blob"
+
+echo
+echo  "tx_blob is: $tx_blob\n"
+echo
+
 curl -X POST -d '{ "method": "submit", "params": [ { "tx_blob": "'"$tx_blob"'" } ] }' http://localhost:5005/
