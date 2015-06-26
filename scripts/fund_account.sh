bb64ef919a2c552872837527446b6c4e5f86c562
crazyquark <sam.bitcoin@gmail.com>
2015-06-26 14:44:04 +0300

    Fund account fix

--- scripts/fund_account.sh
+++ scripts/fund_account.sh
@@ -3,11 +3,11 @@
 ACCOUNT="$1"
 if [ -z $ACCOUNT ]
         then
-                ACCOUNT="rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh"
+                ACCOUNT="r4gzWvzzJS2xLuga9bBc3XmzRMPH3VvxXg"
 fi
 
 command -v js24 > /dev/null || sudo apt-get install libmozjs-24-bin  
-result=`curl -X POST -d '{ "method": "sign", "params": [ { "offline": false, "secret": "masterpassphrase", "tx_json": { "Account": "'"$ACCOUNT"'", "Amount": "999000000", "Destination": "r4gzWvzzJS2xLuga9bBc3XmzRMPH3VvxXg", "TransactionType": "Payment" } } ] }' http://localhost:5005/`
+result=`curl -X POST -d '{ "method": "sign", "params": [ { "offline": false, "secret": "masterpassphrase", "tx_json": { "Account": "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh" , "Amount": "999000000", "Destination": "'"$ACCOUNT"'", "DestinationTag": "1", "TransactionType": "Payment" } } ] }' http://localhost:5005/`
 tx_blob=`echo $result | ./jsawk -j js24 'return this.result.tx_blob'`
 
 echo
@@ -18,7 +18,7 @@ curl -X POST -d '{ "method": "submit", "params": [ { "tx_blob": "'"$tx_blob"'" }
 
 
 echo "Funding with EUR"
-result=`curl -X POST -d '{ "method": "sign", "params": [ { "offline": false, "secret": "masterpassphrase", "tx_json": { "Account": "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh", "Amount": { "currency": "EUR", "issuer" : "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh", "value": "1000" }, "Destination": "'"$ACCOUNT"'", "TransactionType": "Payment" } } ] }' http://localhost:5005/`
+result=`curl -X POST -d '{ "method": "sign", "params": [ { "offline": false, "secret": "shrixAzTwHCDGfYTMUFcqhwdWVzCp", "tx_json": { "Account": "r4gzWvzzJS2xLuga9bBc3XmzRMPH3VvxXg", "Amount": { "currency": "EUR", "issuer" : "r4gzWvzzJS2xLuga9bBc3XmzRMPH3VvxXg", "value": "150" }, "Destination": "'"$ACCOUNT"'", "DestinationTag": "1", "TransactionType": "Payment" } } ] }' http://localhost:5005/`
 
 tx_blob=`echo $result | ./jsawk -j js24 'return this.result.tx_blob'`
 
