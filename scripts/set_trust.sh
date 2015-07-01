aabed2cecc4847f6a19b23c42a34c05dcd222121
crazyquark <sam.bitcoin@gmail.com>
2015-07-01 16:30:54 +0300

    Add set_trust script

---
+++ scripts/set_trust.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Reference here: https://wiki.ripple.com/Sending_RPC_Commands
+ACCOUNT="$1"
+SECRET="$2"
+if [ -z $ACCOUNT ]
+        then
+                ACCOUNT="rNfFpabLoQdemismSSwTWBy5NEtD8RdFHL"
+fi
+if [ -z $SECRET ]
+	then
+		SECRET="ss7UiTtTVJYXfSNGAkTPLDRD8Tmpd"
+fi
+
+curl -v -H "Content-Type: application/json" -X POST --data-binary '{ "secret": "'"$SECRET"'", "trustline": { "limit": "100",  "currency": "EUR",  "counterparty": "r4gzWvzzJS2xLuga9bBc3XmzRMPH3VvxXg", "account_allows_rippling": true } }' http://localhost:5990/v1/accounts/$ACCOUNT/trustlines
